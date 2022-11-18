69c7e387b5daa9255af582dcbe0ac79a1823f077
Owen LeJeune <owen@distillmobile.com>
2022-11-18 17:12:36 -0500

    TVT-63: Add Support for OkHttpProfiler

--- app/src/main/java/com/owenlejeune/tvtime/api/DebugHttpClient.kt
+++ app/src/main/java/com/owenlejeune/tvtime/api/DebugHttpClient.kt
@@ -1,10 +1,12 @@
 package com.owenlejeune.tvtime.api
 
 import com.facebook.stetho.okhttp3.StethoInterceptor
+import com.localebro.okhttpprofiler.OkHttpProfilerInterceptor
 import okhttp3.OkHttpClient
 
 class DebugHttpClient: HttpClient {
     override val httpClient: OkHttpClient = OkHttpClient.Builder()
+        .addInterceptor(OkHttpProfilerInterceptor())
         .addNetworkInterceptor(StethoInterceptor())
         .build()
 }
